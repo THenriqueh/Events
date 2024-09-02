778d4452655d38d974071c41e7b593f708fbd2b3
Thallis <thallis.ferreira2019@gmail.com>
2024-09-01 22:30:33 -0300

    "first commit with entities created"

---
+++ api/src/main/java/com/eventstec/api/domain/address/Address.java
@@ -0,0 +1,30 @@
+package com.eventstec.api.domain.address;
+
+import com.eventstec.api.domain.events.Event;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "address")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Address {
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String city;
+    private String uf;
+
+    @ManyToOne
+    @JoinColumn(name = "event_id")
+    private Event event;
+
+}
---
+++ api/src/main/java/com/eventstec/api/domain/events/Event.java
@@ -0,0 +1,30 @@
+package com.eventstec.api.domain.events;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Table(name = "event")
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Event {
+    @Id
+    @GeneratedValue
+    private UUID id;
+    private String description;
+    private String imgUrl;
+    private String eventUrl;
+    private boolean remote;
+    private Date date;
+}
